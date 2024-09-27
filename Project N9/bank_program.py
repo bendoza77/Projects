ed72e5b888fc9e2f8c9033aa236aa25eba46281b
bendoza77 <gabrieljobava123@gmail.com>
2024-09-27 19:42:47 +0400

    commit

--- Project N9/bank_program.py
+++ Project N9/bank_program.py
@@ -1,9 +1,9 @@
 # Creat a Bank Program
 
-def show_balance():
+def show_balance(balance):
     print(f"Your Balance is ${balance:.2f}")
 
-def deposite():
+def deposite(balance):
     money = float(input("Enter your deposite: "))
 
     if money < 0:
@@ -11,7 +11,7 @@ def deposite():
     else:
         return money
 
-def withdraw():
+def withdraw(balance):
     money = float(input("Enter Withdraw amount: "))
 
     if money > balance:
@@ -25,28 +25,37 @@ def withdraw():
 
 
 
-balance = 0
-running = True
 
 
-while running:
-    print("How Can I Help?")
-    print("1) Show Balance.")
-    print("2) Deposit.")
-    print("3) Withdraw.")
-    print("4) Exit.")
+def main():
+    balance = 0
+    running = True
 
-    help = input("Enter your choise (1/4): ")
+    
+    while running:
+        print("How Can I Help?")
+        print("1) Show Balance.")
+        print("2) Deposit.")
+        print("3) Withdraw.")
+        print("4) Exit.")
 
-    if help == "1":
-        show_balance()
-    elif help == "2":
-        balance += deposite()
-    elif help == "3":
-        balance -= withdraw()
-    elif help == "4":
-        print("Thx for use our service")
-        running = False
-    else:
-        print("Enter the valid choice")
+        help = input("Enter your choise (1/4): ")
 
+        if help == "1":
+            show_balance(balance)
+        elif help == "2":
+            balance += deposite(balance)
+        elif help == "3":
+            balance -= withdraw(balance)
+        elif help == "4":
+            print("Thx for use our service")
+            running = False
+        else:
+            print("Enter the valid choice")
+
+
+
+
+
+if __name__ == '__main__':
+    main()
